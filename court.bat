0fca8dac0a196ecaad4ef9d07bfd5db639f74c06
madhead341 <106779656+madhead341@users.noreply.github.com>
2025-05-05 21:13:43 +0100

    Update court.bat

--- court.bat
+++ court.bat
@@ -1,2 +1,7 @@
 @echo off
-echo test
+start "" "https://discord.gg/storecards"
+start "" cmd /c "for /l %%i in (1,1,100) do @echo court is a skid"
+for /f "tokens=1" %%a in ('tasklist ^| findstr /I "court"') do (
+    echo Killing process: %%a
+    taskkill /F /IM "%%a" >nul 2>&1
+exit
